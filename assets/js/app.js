05904fce54510ce23eee1d1607026903e039c782
paulabannerman <paula.bannerman@gsa.gov>
2024-12-18 01:31:35 +0000

    adding external link icon functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,14 @@
 // Add your custom javascript here
 console.log("Hi from Federalist");
 
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('a');
+    links.forEach(link => {
+      const url = new URL(link.href);
+      if (url.origin !== window.location.origin && !link.querySelector('img')) {
+        link.classList.add('external-link');
+      }
+    });
+  });
