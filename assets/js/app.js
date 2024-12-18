964400585983e023b99355767c21c5087d2cbe1f
paulabannerman <paula.bannerman@gsa.gov>
2024-12-18 15:15:32 +0000

    adding that it opens a new window

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
       const url = new URL(link.href);
       if (url.origin !== window.location.origin && !link.querySelector('img')) {
         link.classList.add('external-link');
+        link.setAttribute('target', '_blank');
       }
     });
   });
