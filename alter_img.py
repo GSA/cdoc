16da4f17eb48ed974093daaa0cda5fc1e5dcd6ab
Christian Attorri <christian.attorri@gsa.gov>
2025-01-10 17:21:13 -0500

    fix slide 4

--- alter_img.py
+++ alter_img.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageDraw
 
-def reposition_white_section(in_, out_, shift_pixels):
+def reposition_box(in_, out_, shift_pixels):
     
     img = Image.open(in_)
     img_width, img_height = img.size
@@ -8,12 +8,20 @@ def reposition_white_section(in_, out_, shift_pixels):
     img = img.convert("RGBA")
     pixels = img.load()
     
+    def is_color_close(pixel, target_color, margin):
+        count = 0
+        for i in range(3):
+            if abs(pixel[i]-target_color[i])<= margin:
+                count+=1
+        return count==3
+    
     
     white_bounds = None
+    target_color = (66, 144, 181, 1)
     for x in range(img_width):
         for y in range(img_height):
             r, g, b, a = pixels[x, y]
-            if (r, g, b) == (255, 255, 255): 
+            if is_color_close((r,g,b,a), target_color, 10): 
                 if not white_bounds:
                     white_bounds = [x, y, x, y] 
                 else:
@@ -28,7 +36,7 @@ def reposition_white_section(in_, out_, shift_pixels):
     x1, y1, x2, y2 = white_bounds
     white_section = img.crop((x1, y1, x2, y2))
     draw = ImageDraw.Draw(img)
-    draw.rectangle([x1, y1, x2, y2], fill=(27,50,82,255))
+    draw.rectangle([x1, y1, x2, y2], fill=(2, 13, 56, 255))
     new_x1 = max(0, x1 - shift_pixels)
     new_x2 = new_x1 + (x2 - x1)
     img.paste(white_section, (new_x1, y1, new_x2, y2), white_section)
@@ -39,6 +47,6 @@ def reposition_white_section(in_, out_, shift_pixels):
 input_img = "input.png"  
 output_img = "output.png" 
 
-shift_pixels = 250
+shift_pixels = 1000
 
-reposition_white_section(input_img, output_img, shift_pixels)
+reposition_box(input_img, output_img, shift_pixels)
